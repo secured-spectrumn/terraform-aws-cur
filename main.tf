19953df555298903cb5aa05826556d8d05591221
David Howell <david@davidjameshowell.com>
2023-03-14 03:29:20 +0000

    Update to bucket versioning to separate resource.

--- main.tf
+++ main.tf
@@ -54,10 +54,6 @@ resource "aws_s3_bucket" "cur" {
   bucket = var.s3_bucket_name
   acl    = "private"
 
-  versioning {
-    enabled = false
-  }
-
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
@@ -68,6 +64,13 @@ resource "aws_s3_bucket" "cur" {
   }
 
   tags = var.tags
+}
+
+resource "aws_s3_bucket_versioning" "cur_versioning" {
+  bucket = aws_s3_bucket.cur.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "cur" {
